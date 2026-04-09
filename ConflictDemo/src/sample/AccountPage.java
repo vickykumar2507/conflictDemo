911a18862f16a87b55bd8f3fb59d03fcd91a004d
vickykumar2507 <surajjha.virat@gmail.com>
2026-04-09 18:58:53 +0530

    accountPage code

---
+++ ConflictDemo/src/sample/AccountPage.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class AccountPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str="this is user 1 account";
+		System.out.println(str);
+	}
+
+}
