0d47847d7adf079c84019db5d1347065e1a459a9
vickykumar2507 <78525942+vickykumar2507@users.noreply.github.com>
2026-04-09 12:40:50 +0530

    Create registerPage.java

---
+++ ConflictDemo/src/sample/registerPage.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class registerPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String rp1="registerPage d2";
+		System.out.println(rp1);
+		
+		
+	}
+
+}
