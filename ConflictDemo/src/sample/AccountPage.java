9c9ce924eb07eaa380303bae20ea1ecc7e66cb96
vickykumar2507 <78525942+vickykumar2507@users.noreply.github.com>
2026-04-09 18:39:18 +0530

    Create AccountPage.java

---
+++ ConflictDemo/src/sample/AccountPage.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class AccountPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str1="this is user 2 account";
+		System.out.println(str1);
+	}
+
+}
